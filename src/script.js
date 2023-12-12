1dad6c72045b0dbc044bcb46d4b621c7749374f9
zyzyzly <zeinabzeinali71@gmail.com>
2023-12-12 22:48:48 +0330

    console errors fixed

--- src/script.js
+++ src/script.js
@@ -2,11 +2,11 @@ var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname) {
-  for (tablinks of tablinks) {
-    tablinks.classList.remove("active-link");
+  for (var link of tablinks) {
+    link.classList.remove("active-link");
   }
-  for (tabcontents of tabcontents) {
-    tabcontents.classList.remove("active-tab");
+  for (var content of tabcontents) {
+    content.classList.remove("active-tab");
   }
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
@@ -17,6 +17,7 @@ var sidemenu = document.getElementById("sidemenu");
 function openmenu() {
   sidemenu.style.right = "0";
 }
+
 function closemenu() {
   sidemenu.style.right = "-200px";
 }
